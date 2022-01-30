3515ee7bf4370fcac597482cce14f1805310a412
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-30 13:54:53 -0300

    Add finished to search repository function

--- src/repositories/AdvertisingRepository.ts
+++ src/repositories/AdvertisingRepository.ts
@@ -6,14 +6,20 @@ import Advertising from '@Entities/AdvertisingEntity'
 type SearchParams = {
   externalId?: string;
   merchantId?: string;
+  finished?: boolean;
 }
 
 @EntityRepository(Advertising)
 export default class AdvertisingRepository extends BaseRepository<Advertising> {
-  search({ externalId, merchantId }: SearchParams = {}) {
+  search({
+    externalId,
+    merchantId,
+    finished
+  }: SearchParams = {}) {
     return this.find({
       ...(externalId ? { externalId } : {}),
       ...(merchantId ? { merchantId } : {}),
+      ...(typeof finished === 'boolean' ? { finished } : {}),
       active: true,
     })
   }
