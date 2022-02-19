a726adc42b085b9f67ef7b0b90c73bf875e00d64
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-19 10:18:06 -0300

    Add route to register favorite

---
+++ src/builders/AdvertisingFavoriteBuilder.ts
@@ -0,0 +1,52 @@
+import Advertising from '@Entities/AdvertisingEntity'
+import AdvertisingFavorite from '@Entities/AdvertisingFavoriteEntity'
+import AlreadyFavoritedError from '@Errors/AlreadyFavoritedError'
+import FinishedAdvertisingError from '@Errors/FinishedAdvertisingError'
+import AdvertisingFavoriteRepository from '@Repositories/AdvertisingFavoriteRepository'
+
+export default class AdvertisingFavoriteBuilder {
+  private _externalId = '';
+  private _advertising: Advertising;
+  private _repository: AdvertisingFavoriteRepository;
+
+  constructor(advertisingFavoriteRepository: AdvertisingFavoriteRepository) {
+    this._repository = advertisingFavoriteRepository
+  }
+
+  setAdvertising(advertising: Advertising) {
+    this._advertising = advertising
+
+    return this
+  }
+
+  setExternalId(externalId: string) {
+    this._externalId = externalId
+
+    return this
+  }
+
+  async validate() {
+    const favoriteWithSameExternalId = await this._repository.search({
+      externalId: this._externalId,
+    })
+
+    if (favoriteWithSameExternalId) {
+      throw new AlreadyFavoritedError()
+    }
+
+    if (this._advertising.finished) {
+      throw new FinishedAdvertisingError()
+    }
+  }
+
+  build = async () => {
+    await this.validate()
+
+    const advertisingFavorite = new AdvertisingFavorite()
+
+    advertisingFavorite.externalId = this._externalId
+    advertisingFavorite.advertising = this._advertising
+
+    return advertisingFavorite
+  }
+}
